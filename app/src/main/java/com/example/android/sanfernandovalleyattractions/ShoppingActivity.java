e462eee1a1f811f18061619c289015cdaa7f4733
Tamas Dober <tamas.dober@mobileforming.com>
2017-07-08 16:23:45 -0700

    Added items to shoppingList

--- app/src/main/java/com/example/android/sanfernandovalleyattractions/ShoppingActivity.java
+++ app/src/main/java/com/example/android/sanfernandovalleyattractions/ShoppingActivity.java
@@ -20,7 +20,12 @@ public class ShoppingActivity extends AppCompatActivity {
         setContentView(R.layout.attraction_list);
 
         final ArrayList<Attraction> shoppingList = new ArrayList<>();
-        shoppingList.add(new Attraction("Santa Susana Pass State Historic Park", "9853 Andora Ave, Chatsworth, CA", new Location("9853", "Andora Ave", "Chatsworth", "CA", "91311"), "Good Thai Food", "http://fpssm.org/history.php",-1));
+        shoppingList.add(new Attraction("T.J. Maxx La Cañada", "663 Foothill Blvd, La Cañada Flintridge, CA 91011", new Location("663", "Foothill Blvd", "La Cañada Flintridge", "CA", "91011"), "Discount retail chain featuring stylish brand-name apparel", "https://www.yelp.com/biz/t-j-maxx-la-canada-flintridge",-1));
+        shoppingList.add(new Attraction("Super King Markets", "19500 Plummer St, Northridge, CA 91324", new Location("19500", "Plummer St", "Northridge", "CA", "91324"), "Supermarket offering groceries & other sundries, a deli, a bakery", "https://superkingmarkets.com/pages/northridge-weekly-ad",-1));
+        shoppingList.add(new Attraction("Porter Ranch Walmart Supercenter", "19821 Rinaldi St, Porter Ranch, CA 91326", new Location("19821", "Rinaldi St", "Porter Ranch", "CA", "91326"), "Garden Center, Pharmacy, Photo Center, Pickup", "https://www.walmart.com/store/2526/details",-1));
+        shoppingList.add(new Attraction("Ralphs Porter Ranch", "19781 Rinaldi St, Porter Ranch, CA 91326", new Location("19781", "Rinaldi St", "Porter Ranch", "CA", "91326"), "They have sushi and sorbet", "https://www.yelp.com/biz/ralphs-los-angeles-70",-1));
+        shoppingList.add(new Attraction("Trader Joe's", "10330 Mason Ave, Chatsworth, CA 91311", new Location("10330", "Mason Ave", "Chatsworth", "CA", "91311"), "Grocery chain with a variety of signature items, plus produce, dairy", "https://www.yelp.com/biz/trader-joes-chatsworth",-1));
+        shoppingList.add(new Attraction("Whole Foods Market", "19340 Rinaldi St, Northridge, CA 91326", new Location("19340", "Rinaldi St", "Northridge", "CA", "91326"), "Eco-minded chain with natural & organic grocery items", "http://www.wholefoodsmarket.com/stores/porterranch",-1));
 
         // Create an {@link AttractionAdapter}, whose data source is a list of {@link Attraction}s.
         // The adapter knows how to create list items for each item in the list.
